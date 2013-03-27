b0b930c70a3399c6fe53f7898bce06200a2c60bb
jingtaot <jingtaot@usc.edu>
2013-03-27 11:43:29 -0700

    Create movie.java
    I set up a separate class for movie object.

---
+++ movie.java
@@ -0,0 +1,18 @@
+package com.facebook.android;
+
+public class Movie {
+  public String imageURL;
+	public String title;
+	public String rating;
+
+	public Movie() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Movie(String imageURL, String title, String rating) {
+		this.imageURL = imageURL;
+		this.title = title;
+		this.rating = rating;
+	}
+
+}
